dc29e359d43bede80301d2b376322f9b4c2e48c9
Mariojung123 <mario@kyonggi.ac.kr>
2024-10-05 14:14:07 +0900

    Create: function conflict_two

--- cal.py
+++ cal.py
@@ -21,7 +21,12 @@ def calculate_and_display(x, y):
     print(f"{x} * {y} = {multiply_result}")
     print(f"{x} / {y} = {divide_result}")
 
+# 충돌을 위한 코드 블럭
+def conflict_two():
+    print("make conflict_two")
+
 # 메인 프로그램 실행
 if __name__ == "__main__":
     x, y = get_numbers()
     calculate_and_display(x, y)
+    conflict_two()
